30385c9bf496af024d44baeac020a6fc9358e40c
horsefacts <webmaster@terminally.online>
2022-10-19 09:20:36 -0400

    Hide uploader when disabled

--- app/components/Upload.tsx
+++ app/components/Upload.tsx
@@ -45,12 +45,7 @@ function Upload({ enabled, onUploadComplete, onError }: UploadProps) {
 
   return (
     <div className="relative">
-      <FileUploader
-        handleChange={handleChange}
-        name="file"
-        types={fileTypes}
-        disabled={!enabled}
-      >
+      {!enabled && (
         <div
           className={`border-2 border-dashed ${border} rounded-xl bg-neutral-100`}
         >
@@ -58,53 +53,70 @@ function Upload({ enabled, onUploadComplete, onError }: UploadProps) {
             {uploadState === "select" && !enabled && (
               <p className="text-neutral-400">Connect wallet to mint</p>
             )}
-            {uploadState === "select" && enabled && (
-              <div>
-                <div className="text-violet-600">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    strokeWidth={1.5}
-                    stroke="currentColor"
-                    className="w-10 h-10 mx-auto"
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      d="M2.25 15.75l5.159-5.159a2.25 2.25 0 013.182 0l5.159 5.159m-1.5-1.5l1.409-1.409a2.25 2.25 0 013.182 0l2.909 2.909m-18 3.75h16.5a1.5 1.5 0 001.5-1.5V6a1.5 1.5 0 00-1.5-1.5H3.75A1.5 1.5 0 002.25 6v12a1.5 1.5 0 001.5 1.5zm10.5-11.25h.008v.008h-.008V8.25zm.375 0a.375.375 0 11-.75 0 .375.375 0 01.75 0z"
-                    />
-                  </svg>
-                </div>
-                <p>Drag & drop an image to upload</p>
-                <p className="text-violet-600 font-bold">or click to browse</p>
-              </div>
-            )}
-            {["finished", "uploading"].includes(uploadState) && (
-              <div>
-                <p>
-                  {objectURI && (
-                    <div className="w-24 h-24 relative mx-auto">
-                      <Image
-                        src={objectURI}
-                        alt="Image preview"
-                        layout="fill"
-                        objectFit="cover"
-                      />
-                    </div>
-                  )}
-                </p>
-                <p>{file && file.name}</p>
-                <p className="font-bold text-violet-600">
-                  {uploadState === "uploading"
-                    ? "Uploading to IPFS..."
-                    : "File saved."}
-                </p>
-              </div>
-            )}
           </div>
         </div>
-      </FileUploader>
+      )}
+      {enabled && (
+        <FileUploader
+          handleChange={handleChange}
+          name="file"
+          types={fileTypes}
+          disabled={!enabled}
+        >
+          <div
+            className={`border-2 border-dashed ${border} rounded-xl bg-neutral-100`}
+          >
+            <div className="p-16 text-center cursor-pointer">
+              {uploadState === "select" && enabled && (
+                <div>
+                  <div className="text-violet-600">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      strokeWidth={1.5}
+                      stroke="currentColor"
+                      className="w-10 h-10 mx-auto"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="M2.25 15.75l5.159-5.159a2.25 2.25 0 013.182 0l5.159 5.159m-1.5-1.5l1.409-1.409a2.25 2.25 0 013.182 0l2.909 2.909m-18 3.75h16.5a1.5 1.5 0 001.5-1.5V6a1.5 1.5 0 00-1.5-1.5H3.75A1.5 1.5 0 002.25 6v12a1.5 1.5 0 001.5 1.5zm10.5-11.25h.008v.008h-.008V8.25zm.375 0a.375.375 0 11-.75 0 .375.375 0 01.75 0z"
+                      />
+                    </svg>
+                  </div>
+                  <p>Drag & drop an image to upload</p>
+                  <p className="text-violet-600 font-bold">
+                    or click to browse
+                  </p>
+                </div>
+              )}
+              {["finished", "uploading"].includes(uploadState) && (
+                <div>
+                  <p>
+                    {objectURI && (
+                      <div className="w-24 h-24 relative mx-auto">
+                        <Image
+                          src={objectURI}
+                          alt="Image preview"
+                          layout="fill"
+                          objectFit="cover"
+                        />
+                      </div>
+                    )}
+                  </p>
+                  <p>{file && file.name}</p>
+                  <p className="font-bold text-violet-600">
+                    {uploadState === "uploading"
+                      ? "Uploading to IPFS..."
+                      : "File saved."}
+                  </p>
+                </div>
+              )}
+            </div>
+          </div>
+        </FileUploader>
+      )}
     </div>
   );
 }
